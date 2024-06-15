ea156a4642d6f11e44ccadb7d054f37e972f417e
Gian Paloso <18108@rosmini.school.nz>
2024-06-15 14:35:18 +1200

    Upgraded Procedural Generation by ALOT
    - Created Original Designs
    - Added Terrain
    - Added different ground textures (rng)
    
    Other Things
    - added zooming capability
    - created a tileset data set

--- real game/Actors/Players/player_base.gd
+++ real game/Actors/Players/player_base.gd
@@ -4,11 +4,23 @@ signal health_depleted
 
 var health = 100.0
 
+@onready var camera_2d = $Camera2D
+
 func _input(event):
 	
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		move_to_position(get_global_mouse_position())
-
+		
+	if Input.is_action_just_pressed("zoom_in"):
+		var zoom_val =camera_2d.zoom.x + 0.1
+		
+		camera_2d.zoom = Vector2(zoom_val, zoom_val)
+	elif Input.is_action_just_pressed("zoom_out"):
+		var zoom_val =camera_2d.zoom.x - 0.1
+		if zoom_val == 0:
+			zoom_val =camera_2d.zoom.x - 0.2
+			
+		camera_2d.zoom = Vector2(zoom_val, zoom_val)
 		
 func _physics_process(delta):
 	move(delta)
